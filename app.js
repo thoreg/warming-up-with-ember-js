b1914f8dcba07e88956fcf56ec6b74f1b2dcfdc6
Thomas Rega <thomas.rega@momox.biz>
2014-02-27 08:02:26 +0100

    Put contact data into fixture

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
-
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
-
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.resource('products', function() {
     this.resource('product', { path: '/:product_id' });
   });
   this.resource('contacts', function() {
-    this.resource('contact', { path: '/:name' });
+    this.resource('contact', { path: '/:contact_id' });
   });
 });
-
-App.Product = DS.Model.extend({
-	title: DS.attr('string'),
-	price: DS.attr('number'),
-	description: DS.attr('string'),
-	isOnSale: DS.attr('boolean'),
-	image: DS.attr('string'),
-});
-
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
   logo: 'images/logo-small.png',
@@ -42,21 +30,28 @@ App.ProductsRoute = Ember.Route.extend({
     return this.store.findAll('product');
   }
 });
-
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
-    return App.CONTACTS;
+    return this.store.findAll('contact');
   }
 });
 App.ContactRoute = Ember.Route.extend({
   model: function(params) {
-    return App.CONTACTS.findBy('name', params.name);
+    return this.store.find('contact', params.contact_id);
   }
 });
 
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string')
+});
 App.Product.FIXTURES = [
   {
-    id:1,
+    id: 1,
     title: 'Flint',
     price: 99,
     description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
@@ -64,7 +59,7 @@ App.Product.FIXTURES = [
     image: 'images/products/flint.png'
   },
   {
-    id:2,
+    id: 2,
     title: 'Kindling',
     price: 249,
     description: 'Easily combustible small sticks or twigs used for starting a fire.',
@@ -73,13 +68,21 @@ App.Product.FIXTURES = [
   }
 ];
 
-App.CONTACTS = [
+App.Contact = DS.Model.extend({
+	name: DS.attr('string'),
+  about: DS.attr('string'),
+  avatar: DS.attr('string')
+});
+
+App.Contact.FIXTURES = [
   {
+    id:1,
     name: 'Giamia',
     about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
     avatar: 'images/contacts/giamia.png'
   },
   {
+    id:2,
     name: 'Anostagia',
     about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
     avatar: 'images/contacts/anostagia.png'
