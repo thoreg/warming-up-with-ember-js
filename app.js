5d33641855fc7f90c22d6e90c859f72812875b1b
Thomas Rega <thomas.rega@momox.biz>
2014-02-27 13:49:43 +0100

    Add contact as relation to product and link them together

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ App.Router.map(function() {
     this.resource('contact', { path: '/:contact_id' });
   });
 });
+
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
   logo: 'images/logo-small.png',
@@ -43,36 +44,19 @@ App.Product = DS.Model.extend({
   description: DS.attr('string'),
   isOnSale: DS.attr('boolean'),
   image: DS.attr('string'),
-  reviews: DS.hasMany('review', {async: true})
+  reviews: DS.hasMany('review', { async: true }),
+  crafter: DS.belongsTo('contact', { async: true })
 });
-App.Review = DS.Model.extend({
-  text: DS.attr('string'),
-  reviewedAt: DS.attr('date'),
-  product: DS.belongsTo('product')
-});
-App.Review.FIXTURES = [
-  {
-    id: 1,
-    product: 1,
-    text: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
-    reviewedAt: '2014-02-27',
-  },
-  {
-    id: 2,
-    product: 1,
-    text: 'Easily combustible small sticks or twigs used for starting a fire.',
-    reviewedAt: '2014-02-21'
-  }
-];
+
 App.Product.FIXTURES = [
-  {
-    id: 1,
+ {  id: 1,
     title: 'Flint',
     price: 99,
     description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
     isOnSale: true,
     image: 'images/products/flint.png',
-    reviews: [1,2]
+    reviews: [100,101],
+    crafter: 200
   },
   {
     id: 2,
@@ -80,27 +64,49 @@ App.Product.FIXTURES = [
     price: 249,
     description: 'Easily combustible small sticks or twigs used for starting a fire.',
     isOnSale: false,
-    image: 'images/products/kindling.png'
+    image: 'images/products/kindling.png',
+    reviews: [],
+    crafter: 200
   }
 ];
 
 App.Contact = DS.Model.extend({
   name: DS.attr('string'),
   about: DS.attr('string'),
-  avatar: DS.attr('string')
+  avatar: DS.attr('string'),
+  products: DS.hasMany('product', { async: true })
 });
+
 App.Contact.FIXTURES = [
   {
     id: 200,
     name: 'Giamia',
     about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
-    avatar: 'images/contacts/giamia.png'
+    avatar: 'images/contacts/giamia.png',
+		products: [1,2]
   },
   {
     id: 201,
     name: 'Anostagia',
     about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
-    avatar: 'images/contacts/anostagia.png'
+    avatar: 'images/contacts/anostagia.png',
+    products: []
+  }
+];
+
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date'),
+  product: DS.belongsTo('product')
+});
+App.Review.FIXTURES = [
+  {
+    id: 100,
+    text: "Started a fire in no time!"
+  },
+  {
+    id: 101,
+    text: "Not the brightest flame, but warm!"
   }
 ];
 
