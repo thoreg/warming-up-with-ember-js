57631ccc024aa84a0ee318e45303a1eae3df1c8f
Thomas Rega <thomas.rega@momox.biz>
2014-02-28 15:33:21 +0100

    Add ReviewView as exercise

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ App.ProductController = Ember.ObjectController.extend({
         controller.get('model.reviews')
                   .addObject(review);
       });
-    },
+    }
   }
 });
 
@@ -95,6 +95,17 @@ App.ProductsDealsRoute = Ember.Route.extend({
       return product.get('price') < 500;
     });
   }
+});
+
+App.ReviewView = Ember.View.extend({
+  isExpanded: false,
+  classNameBindings: ['isExpanded', 'readMore'],
+  click: function(){
+    this.toggleProperty('isExpanded');
+  },
+  readMore: function(){
+    return this.get('length') > 140;
+  }.property('length')
 });
 
 App.ProductDetailsComponent = Ember.Component.extend({
@@ -226,12 +237,20 @@ App.Review = DS.Model.extend({
 App.Review.FIXTURES = [
   {
     id: 100,
+    reviewedAt: new Date('12/10/2013').getTime(),
     text: "Started a fire in no time!",
     rating: 4
   },
   {
     id: 101,
+    reviewedAt: new Date('12/12/2013').getTime(),
     text: "Not the brightest flame, but warm!",
+    rating: 5
+  },
+  {
+    id: 102,
+    reviewedAt: new Date('12/30/2013').getTime(),
+    text: "This is some amazing Flint! It lasts **forever** and works even when damp! I still remember the first day when I was only a little fire sprite and got one of these in my flame stalking for treemas. My eyes lit up the moment I tried it! Here's just a few uses for it:\n\n* Create a fire using just a knife and kindling!\n* Works even after jumping in a lake (although, that's suicide for me)\n* Small enough to fit in a pocket -- if you happen to wear pants\n\n\nYears later I'm still using the _same one_. That's the biggest advantage of this -- it doesn't run out easily like matches. As long as you have something to strike it against, **you can start a fire anywhere** you have something to burn!",
     rating: 5
   }
 ];
